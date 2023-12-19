a59bfed533e1f2fd4b9226782f6e9f4b5b4a3c7d
Ziv Caspi <zivc@microsoft.com>
2023-12-19 16:39:53 +0200

    Page: Page through a huge file one screen at a time. Can also give a pattern to start at (ignore all data before the pattern)

---
+++ page/FindSubstringInStream.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region class FindSubstringInStream
+public static class FindSubstringInStream
+{
+    /// <summary>
+    /// Locates the substring <paramref name="substring"/> in a stream of characters produced by <paramref name="getNextCharacterOrMinusOne"/>.
+    /// </summary>
+    /// <param name="getNextCharacterOrMinusOne">A function that returns the stream of characters as integers (char values),
+    /// yielding -1 when there are no other characters to return.</param>
+    /// <param name="substring">The substring to search for.</param>
+    /// <returns>The first position (0-based) in the stream at which the first matching substring can be found.
+    /// -1 is returned if there's no match.</returns>
+    /// <remarks>
+    /// Trivial implementation using DFA</remarks>
+    public static long IndexOf(Func<int> getNextCharacterOrMinusOne, string substring)
+    {
+        // TODO: Validate stream, substring make sense
+        var n = substring.Length;
+
+        var prev = new Indexes();
+        prev.Init(n);
+
+        var next = new Indexes();
+        next.Init(n);
+
+        var pos = -1;
+        while (true)
+        {
+            var ch = getNextCharacterOrMinusOne();
+            pos++;
+            if (ch <= 0)
+            {
+                return -1;
+            }
+
+            prev.Push(0);
+            foreach (var i in prev)
+            {
+                if (ch == substring[i])
+                {
+                    if (i + 1 == n)
+                    {
+                        return pos + 1 - n;
+                    }
+                    else
+                    {
+                        next.Push(i + 1);
+                    }
+                }
+            }
+            prev.CopyFromAndClear(ref next);
+        }
+    }
+
+    private struct Indexes : IEnumerable<int>
+    {
+        public int[] m_array;
+        public int m_count;
+
+        public void Init(int n)
+        {
+            m_array = new int[n];
+            m_count = 0;
+        }
+
+        public void Push(int i)
+        {
+            m_array[m_count++] = i;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < m_count; i++)
+            {
+                yield return m_array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new Exception();
+        }
+
+        public void CopyFromAndClear(ref Indexes rhs)
+        {
+            var temp = rhs.m_array;
+
+            m_array = rhs.m_array;
+            m_count = rhs.m_count;
+
+            rhs.m_array = temp;
+            rhs.m_count = 0;
+        }
+    }
+
+    public static void Test()
+    {
+        var pos = IndexOf(Produce("bbbaaa"), "aaa");
+        // pos here should be 3 (position at which the match started)
+    }
+
+    private static Func<int> Produce(string s)
+    {
+        var pos = 0;
+        return () =>
+        {
+            if (pos < s.Length)
+            {
+                return s[pos++];
+            }
+            return -1;
+        };
+    }
+}
+#endregion
+
---
+++ page/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+static class Program
+{
+    private static StringBuilder sb = new StringBuilder();
+    private static string pattern = null;
+
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+        {
+            return Usage();
+        }
+
+        if (!File.Exists(args[0]))
+        {
+            Console.WriteLine("File does not exist.");
+            return 1;
+        }
+
+        if (Console.IsOutputRedirected)
+        {
+            Console.WriteLine("Input is redirected, why use page?");
+            return 2;
+        }
+
+        if (args.Length > 1 && !string.IsNullOrWhiteSpace(args[1]))
+        {
+            pattern = args[1];
+        }
+
+        var nLines = Math.Max(Console.WindowHeight, 4);
+        var width = Math.Max(Console.WindowWidth, 4);
+        using (var file = File.OpenRead(args[0]))
+        {
+            using (var reader = new StreamReader(file, detectEncodingFromByteOrderMarks: true, bufferSize: 4096))
+            {
+                if (!string.IsNullOrEmpty(pattern))
+                {
+                    if (!ReadUntilPattern(reader, pattern))
+                    {
+                        Console.WriteLine(">>> Pattern not found <<<");
+                        return 0;
+                    }
+                }
+
+                var done = false;
+                while (!done)
+                {
+                    for (int i = 0; i < nLines; i++)
+                    {
+                        var line = ReadLine(reader, width);
+                        if (line == null)
+                        {
+                            done = true;
+                            break;
+                        }
+                        Console.WriteLine(line);
+                    }
+
+                    if (!done)
+                    {
+                        // TODO: Just wait for input... In the future we'll remove this line and continue again
+                        Console.WriteLine();
+                        Console.WriteLine(">>> Press ENTER to continue <<<");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static bool ReadUntilPattern(TextReader reader, string pattern)
+    {
+        var pos = FindSubstringInStream.IndexOf(reader.Read, pattern);
+        return pos != -1;
+    }
+
+    public static int Usage()
+    {
+        Console.WriteLine("Usage: page <filename> [<pattern>]");
+        return 0;
+    }
+
+    private static string? ReadLine(TextReader reader, int width)
+    {
+        sb.Clear();
+        for (int i = 0; i < width; i++)
+        {
+            var ch = reader.Read();
+            if ( ch <= 0)
+            {
+                return null;
+            }
+
+            if (ch == '\r' || ch == '\n')
+            {
+                break;
+            }
+            sb.Append((char)ch);
+        }
+        return sb.ToString();
+    }
+}
+
+
