33cd0ef1e5ae42b34af33d813725172d75d6d04d
Ziv Caspi <zivc@microsoft.com>
2025-12-16 08:45:00 +0200

    ztime: Report how long it takes to run a command

---
+++ ztime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    // This utility measures how long it takes to execution the provided command.
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: ztime <command> [args...]");
+            return;
+        }
+
+        Console.WriteLine($"ztime: Starting...");
+        var sw = System.Diagnostics.Stopwatch.StartNew();
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = args[0],
+                Arguments = string.Join(" ", args, 1, args.Length - 1), // TODO: Escape arguments
+                UseShellExecute = false
+            }).WaitForExit();
+        }
+        finally
+        {
+            Console.WriteLine($"ztime: Elapsed time: {sw.Elapsed}");
+        }
+    }
+}
+
+
