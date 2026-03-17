91614641442aa1eb5a7384163aaf7e62156ccad1
Ziv Caspi <zivc@microsoft.com>
2026-03-17 14:48:51 +0200

    show_args: Added utility to show command-line args and redirected input

---
+++ show_args/Program.cs
@@ -0,0 +1,65 @@
+﻿// A debugging program that echoes the piped input (if any) and each of its arguments (if any)
+using System;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        if (Console.IsInputRedirected)
+        {
+            WriteRule("Redirected input:");
+            while (true)
+            {
+                var line = Console.In.ReadLine();
+                if  (line == null)
+                {
+                    break;
+                }
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            WriteRule("(Input is not redirected)");
+        }
+
+
+        if (args != null && args.Length > 0)
+        {
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                WriteRule($"arg[{i}]:");
+
+                Console.WriteLine(args[i]);
+            }
+        }
+        else
+        {
+            WriteRule("(No command-line arguments)");
+        }
+
+        WriteRule("(Done)");
+
+        return 0;
+    }
+
+    private static void WriteRule(string text = null)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            Console.WriteLine(new string('-', 80));
+            return;
+        }
+
+        if (text.Length > 70)
+        {
+            text = text.Substring(0, 70);
+        }
+
+        Console.Write(new string('-', 80 - text.Length - 3));
+        Console.Write(' ');
+        Console.Write(text);
+        Console.WriteLine(" -");
+    }
+}
